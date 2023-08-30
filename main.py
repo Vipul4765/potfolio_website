f79afc87efe1b052fd19da54ccc18004017e0929
Vipul Kumar <vipuldhankar17170277@gmail.com>
2023-08-30 12:13:06 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import pandas as pd
+from streamlit.runtime.state import widgets
+from streamlit_option_menu import option_menu
+import requests
+from streamlit_lottie import st_lottie
+
+st.set_page_config(layout='wide')
+
+
+def load_lottie1(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+lottie_coder = load_lottie1('https://lottie.host/69aed16c-7cf5-46b9-beb6-50aee75e3535/1IH5t9bwIN.json')
+
+st.write('##')
+st.subheader('I am vipul Kumar')
+st.title('My portfolio Website')
+st.write('-----')
+
+with st.container():
+    selected = option_menu(
+        menu_title=None,
+        options=['About', 'project', 'Certification', 'Contact'],
+        icons=['person', 'code-slash', 'cc-square', 'chat-left-text-fill'],
+        orientation='horizontal'
+
+    )
+if selected == 'About':
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader('🎓Undergraduate At SRM University')
+            st.write("""Welcome! I'm a Python enthusiast skilled in utilizing essential libraries like NumPy, Pandas, 
+            Matplotlib, Seaborn, and MySQL. Through various projects, I've harnessed these tools to analyze data and 
+            create insightful visualizations. My passion lies in unraveling the narratives within datasets, 
+            approaching challenges with a detail-oriented and solution-driven mindset. I navigate the 
+            dynamic world of data, consistently seeking innovative ways to contribute and grow.""")
+            st.write("----------")
+            skills = ['Python', 'MySql', 'Numpy', 'Pandas', 'Matplotlib', 'Seaborn', 'C++', 'DSA', 'OOPs', 'Streamlit',
+                      'Flask', 'Html']
+            # Apply styling for the button-like structure
+            button_style = "background-color: orange; border: none; color: white; padding: 8px 20px; margin: 5px; cursor: pointer;"
+
+            # Display the Skills section
+            st.title('🔥Skills')
+
+            # Display skills buttons in rows
+            current_row = []
+            for skill in skills:
+                button_markup = f'<button style="{button_style}">{skill}</button>'
+                current_row.append(button_markup)
+
+                # Display 3 buttons in a row
+                if len(current_row) == 3:
+                    st.markdown(" ".join(current_row), unsafe_allow_html=True)
+                    current_row = []
+
+            # Display any remaining buttons
+            if current_row:
+                st.markdown(" ".join(current_row), unsafe_allow_html=True)
+
+        with col2:
+            with col2:
+                col1, col2 = st.columns(2)
+                with col2:
+                    st_lottie(lottie_coder, width=350, )  # Adjust width and alignment
+                st.write('---')
+                st.header("👨‍💻Coding Profile")
+                st.markdown("- [GFG](https://auth.geeksforgeeks.org/user/vipuldhankar)")
+                st.markdown("- [Leetcode](https://leetcode.com/coder_123_/)")
+                st.markdown("- [CodeChef](https://www.codechef.com/users/vipuldhankar17)")
+                st.markdown("- [Hackerrank](https://www.hackerrank.com/vipuldhankar1711)")
+                st.markdown(
+                    "- [Coding Ninja](https://www.codingninjas.com/studio/profile/a732a5a7-88e5-44ab-9eaf-4af8a4cb59ff)")
+
+        st.write('-----')
+        with st.container():
+            col3, col4 = st.columns(2)
+            with col3:
+                st.header("Education")
+                education_df = pd.DataFrame({
+                    'Education': ['B.Tech', 'XII', 'X'],
+                    'Passing Year': [2024, 2019, 2017],
+                    'Grade': ['8.86 CGPA', '66.6%', '7.8 CGPA'],
+                    'Board': ['SRMIST', 'CBSE', 'CBSEs']
+                })
+                st.table(education_df.style.set_properties(**{'text-align': 'center', 'width': 'auto'}))
+
+            with col4:
+                st.write('#')
+                st.write('#')
+                st.code("""
+                passion = "I find my passion in coding and problem-solving!"
+                print(passion)
+""")
+    st.write('##')
+    with st.container():
+        col5, col6 = st.columns(2)
+
+        pass
+
+if selected == 'Certification':
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.container():
+            selected = option_menu(
+                menu_title="Please choose from the options below and click to view the awarded certificate",
+                options=['Python', 'DSA', 'Data Analysis', 'Pandas', 'OOPs'],
+                icons=['code-square', 'code-square', 'code-square', 'code-square', 'code-square'],
+            )
+
+        if selected == 'Python':
+            with col2:
+                st.image('CN_Certificate_914cbfc05b28dfd9 (1)_page-0001.jpg', caption='Coding Ninjas Certificate',
+                         use_column_width=True)
+
+        if selected == 'DSA':
+            with col2:
+                st.image('NPTEL DSA_page-0001.jpg')
+        if selected == 'Data Analysis':
+            with col2:
+                st.image('Data Analysis_page-0001.jpg')
+        if selected == 'Pandas':
+            with col2:
+                st.image('Pandas_page-0001.jpg')
+        if selected == 'OOPs':
+            with col2:
+                st.write('no available yet')
+
+st.write('----')
+if selected == 'project':
+    with st.container():
+        selected = option_menu(
+            menu_title='select one ',
+            options=['Type1 Project', 'Type1 Project', 'Type3 Project'],
+            icons=['file-code', 'file-code', 'file-code'],
+            orientation='horizontal'
+        )
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('vipul')
